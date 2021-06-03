0317ec850c5183cbab75aa5a323a9c30fb888ef9
Václav Smítka <va.smitka@gmail.com>
2021-06-03 12:39:25 +0200

    all services can work itself

--- mongo-init/mongo-init.sh
+++ mongo-init/mongo-init.sh
@@ -17,4 +17,32 @@ db.createUser({
     pwd: '$DB_NOTIFICATION_PASSWORD',
     roles: [{ role: 'readWrite', db: '$NOTIFICATION_DATABASE_NAME' }],
 });
+db.createCollection('configurations', { capped: false });
+db.configurations.insert({
+  'type':'TEST_MAIL_NOTIFICATION',
+  'notificatorName':'MailNotificator',
+  'notificatorParameters':{
+    'from':'$FROM_MAIL_ADDRESS',
+    'subject': 'Hello there'
+  },
+  'template':'Dear \${name}, hi. Thanks, bye :)'
+});
+db.configurations.insert({
+  'type':'JOB_OFFER_CREATED',
+  'notificatorName':'MailNotificator',
+  'notificatorParameters':{
+    'from':'$FROM_MAIL_ADDRESS',
+    'subject': 'New marketplace offer!'
+  },
+  'template':'There is a new marketplace offer from company \${companyName}: \n\${description}.'
+});
+db.configurations.insert({
+  'type':'NEW_USER_ACCOUNT_REGISTERED',
+  'notificatorName':'MailNotificator',
+  'notificatorParameters':{
+    'from':'$FROM_MAIL_ADDRESS',
+    'subject': 'Welcome'
+  },
+  'template':'Dear \${name}, thank you for your registration. Bye :)'
+});
 EOF
